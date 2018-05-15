0856cb7dfe3f77ffb171968c77077c4f7e4308f3
Kenneth Korcal <kakorcal@gmail.com>
2018-05-14 17:25:07 -0700

    adding colors, buttons, heros

--- client/js/pages/Landing.jsx
+++ client/js/pages/Landing.jsx
@@ -3,17 +3,68 @@ import { Link } from 'react-router-dom';
 
 const Landing = () => (
   <div className="page page__landing">
-    <div className="hero__container illustration__overlay">
-      <div className="hero__heading">
-        <h1 className="hero__title">
-          <Link to="/">Kanji Master</Link>
-        </h1>
-        <p className="hero__subtitle">The Ultimate Path To Kanji Mastery</p>
-        <div className="hero__button">
-          <Link to="/test">Get Started For Free</Link>
+    <div className="hero__container">
+      <div className="hero__douty illustration__douty__full" />
+      <div className="hero__overlay illustration__overlay">
+        <div className="container">
+          <div className="hero__heading">
+            <h1 className="hero__title">
+              <Link to="/">Kanji Master</Link>
+            </h1>
+            <p className="hero__subtitle">The Ultimate Path To Kanji Mastery</p>
+            <div className="button button--green button--large">
+              <Link to="/test">Get Started For Free</Link>
+            </div>
+          </div>
+          <div className="hero__nav">
+            <div className="hero__nav__item button button--pink button--medium">
+              <Link to="/test">Login</Link>
+            </div>
+            <div className="hero__nav__item button button--blue button--medium">
+              <Link to="/test">Signup</Link>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="hero__douty illustration__douty--full" />
+    </div>
+    <div className="container">
+      <div className="content">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque esse, eveniet doloribus totam quis error
+          facere in quam perspiciatis fugiat hic maxime molestias deleniti. Ratione expedita vel optio libero
+          consequuntur?
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque esse, eveniet doloribus totam quis error
+          facere in quam perspiciatis fugiat hic maxime molestias deleniti. Ratione expedita vel optio libero
+          consequuntur?
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque esse, eveniet doloribus totam quis error
+          facere in quam perspiciatis fugiat hic maxime molestias deleniti. Ratione expedita vel optio libero
+          consequuntur?
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque esse, eveniet doloribus totam quis error
+          facere in quam perspiciatis fugiat hic maxime molestias deleniti. Ratione expedita vel optio libero
+          consequuntur?
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque esse, eveniet doloribus totam quis error
+          facere in quam perspiciatis fugiat hic maxime molestias deleniti. Ratione expedita vel optio libero
+          consequuntur?
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque esse, eveniet doloribus totam quis error
+          facere in quam perspiciatis fugiat hic maxime molestias deleniti. Ratione expedita vel optio libero
+          consequuntur?
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque esse, eveniet doloribus totam quis error
+          facere in quam perspiciatis fugiat hic maxime molestias deleniti. Ratione expedita vel optio libero
+          consequuntur?
+        </p>
+      </div>
     </div>
   </div>
 );
